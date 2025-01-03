c490d4094385217a9488692f82db0a53ed6b66ca
mukkamukka <mukkamukka@github.com>
2025-01-03 20:20:24 +0900

    Solved 2557 / Bronze V

---
+++ 백준/단계별/chapter01/BOJ_2557.java
@@ -0,0 +1,17 @@
+package 단계별.chapter01;
+
+/* https://www.acmicpc.net/problem/2557 */
+
+import java.io.*;
+
+public class BOJ_2557 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String s = "Hello World!";
+
+        bw.write(s);
+        bw.flush();
+        bw.close();
+    }
+}
