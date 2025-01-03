e421531e35abc261b30d0d3631376cf512950118
mukkamukka <rlaehdrjs4605@gmail.com>
2025-01-03 20:22:08 +0900

    Solved 1001 / Bronze V

---
+++ 백준/단계별/chapter01/BOJ_1001.java
@@ -0,0 +1,24 @@
+package 단계별.chapter01;
+
+/* https://www.acmicpc.net/problem/1000 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(a - b);
+        System.out.print(sb);
+    }
+}
