016d7173cd8a745c7691517a15806a0e693f13fe
mukkamukka <rlaehdrjs4605@gmail.com>
2025-01-04 17:59:00 +0900

    Solved 10950 / Bronze V

---
+++ 백준/단계별/chapter03/BOJ_10950.java
@@ -0,0 +1,25 @@
+package 단계별.chapter03;
+
+/* https://www.acmicpc.net/problem/10950 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_10950 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < t; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a + b).append("\n");
+        }
+        br.close();
+        System.out.print(sb);
+    }
+}
