c73d6b37c41e88f4e45782049f9c9f74bb037c51
mukkamukka <rlaehdrjs4605@gmail.com>
2025-01-05 21:17:06 +0900

    Solved 2438 / Bronze V

---
+++ 백준/단계별/chapter03/BOJ_2438.java
@@ -0,0 +1,22 @@
+package 단계별.chapter03;
+
+/* https://www.acmicpc.net/problem/2438 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            sb.append("*".repeat(i));
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
