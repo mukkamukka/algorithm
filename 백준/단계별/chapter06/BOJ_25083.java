cf67c14f8167e8412a68c672c077b41bad51554c
mukkamukka <rlaehdrjs4605@gmail.com>
2025-01-07 23:41:40 +0900

    Solved 25083 / Bronze V

---
+++ 백준/단계별/chapter06/BOJ_25083.java
@@ -0,0 +1,16 @@
+package 단계별.chapter06;
+
+/* https://www.acmicpc.net/problem/25083 */
+
+public class BOJ_25083 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7").append("\n");
+        sb.append("r`-_   ,'  ,/").append("\n");
+        sb.append(" \\. \". L_r'").append("\n");
+        sb.append("   `~\\/").append("\n");
+        sb.append("      |").append("\n");
+        sb.append("      |");
+        System.out.print(sb);
+    }
+}
