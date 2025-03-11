391c63c3a85923793ae3b75d72569b11d23584fd
mukkamukka <rlaehdrjs4605@gmail.com>
2025-03-12 00:00:54 +0900

    Solved 10870 / Bronze II

---
+++ 백준/단계별/chapter21/BOJ_10870.java
@@ -0,0 +1,24 @@
+package 단계별.chapter21;
+
+/* https://www.acmicpc.net/problem/10870 */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        System.out.print(fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
