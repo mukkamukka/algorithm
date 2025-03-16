73e123d6deda09a88811181cf877aa80e7e95cef
mukkamukka <rlaehdrjs4605@gmail.com>
2025-03-16 23:31:45 +0900

    Solved 11047 / Silver IV

--- 백준/분류별/greedy/BOJ_11047.java
+++ 백준/분류별/greedy/BOJ_11047.java
@@ -11,6 +11,7 @@ public class BOJ_11047 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
         int[] coins = new int[n];
