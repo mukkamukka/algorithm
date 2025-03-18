1acfcb33032f2d714e6c3a136659897b8968bcd4
mukkamukka <rlaehdrjs4605@gmail.com>
2025-03-18 20:16:38 +0900

    Solved 16953 / Silver II

--- 백준/분류별/greedy/BOJ_16953.java
+++ 백준/분류별/greedy/BOJ_16953.java
@@ -12,8 +12,8 @@ public class BOJ_16953 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        long a = Integer.parseInt(st.nextToken());
-        long b = Integer.parseInt(st.nextToken());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
 
         int cnt = 1;
         while (b != a) {
